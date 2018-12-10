c246877219e31611652eb16dc8a74d4716260076
Atif Ali <atif@monotote.com>
2018-12-10 16:07:51 +0500

    Updating

--- app/Http/Controllers/ProjectsController.php
+++ app/Http/Controllers/ProjectsController.php
@@ -9,7 +9,7 @@ class ProjectsController extends Controller
 {
     public function index(){
 
-        $projects = Project::all();
+        $projects = Project::where('owner_id', auth()->id())->get();
         return view('projects.index', compact('projects'));
     }
 
